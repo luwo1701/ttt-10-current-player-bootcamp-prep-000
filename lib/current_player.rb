2ed7f215664e104b8f8e536bd2c933ff095385f4
Luke Worley <luke.worley@colorado.edu>
2018-05-21 18:51:47 +0000

    automatically backed up by learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,6 +1,7 @@
 def turn_count(board)
   board.each do |turn|
-    if turn != "" || turn != " " || turn != nile
+    if turn != "" || turn != " " || turn != nil
+      count += 1
 end
 
 def current_player(board)
